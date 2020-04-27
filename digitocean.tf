de45b2cc881d7a5351d81d5aa55e272ac9de3cb2
Valay Patel <valay.patel@onlia.ca>
2020-04-27 13:19:08 -0400

    Test Commit

---
+++ digitocean.tf
@@ -0,0 +1,13 @@
+# Set the variable value in *.tfvars file
+# or using -var="do_token=..." CLI option
+variable "do_token" {}
+
+# Configure the DigitalOcean Provider
+provider "digitalocean" {
+  token = "${var.do_token}"
+}
+
+# Create a web server
+resource "digitalocean_droplet" "web" {
+  # ...
+}
